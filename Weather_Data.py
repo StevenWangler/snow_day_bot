6d1e837881738a0c53d2082bbcb4c3c5ffbc8cb8
Steven Wangler <swangler@bdo.com>
2023-03-10 13:11:30 -0500

    first commit

---
+++ Weather_Data.py
@@ -0,0 +1,7 @@
+#put a description here
+
+def get_relevant_weather_information(forecast_data):
+    # Get the value of "mintemp_f"
+    mintemp_f = forecast_data['forecast']['forecastday'][0]['day']['mintemp_f']
+    print(mintemp_f)
+    
---
+++ settings.py
@@ -0,0 +1,6 @@
+#Settings.py
+import os
+
+WEATHER_API_KEY = os.getenv('WeatherAPI_Key')
+ZIP_CODE = '49341'
+WEATHER_API_BASE = 'http://api.weatherapi.com/v1/'
